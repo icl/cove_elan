66572ac7fdbe32ab669c6aeee0b5636d89ac9aac
Aaron Hunter <aaron@hunter.cx>
2012-01-17 15:27:02 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoveElan::Application.config.secret_token = 'db582f4b877cca1f87de33e8afd07a4ef204a41d205f0112037b312dd18c133ad85331bceac81ad630e6c1ff73b4bf397476b97a63d4fa1431cc8e2eadd62be7'
