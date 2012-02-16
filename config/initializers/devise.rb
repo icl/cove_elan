254d2f65e6f4f0f94cd03785f2ced3163f2366cf
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-02-15 19:56:45 -0800

    explicitly set devise router

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,6 +6,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
   config.mailer_sender = "please-change-me-at-config-initializers-devise@example.com"
 
+  Devise.router_name = :main_app
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
 
