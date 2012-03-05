f70d87c66a4ee45f38f4ccf83777421ada135c9e
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-03-05 09:23:17 -0800

    move viewer right

--- app/assets/javascripts/document_viewer.js
+++ app/assets/javascripts/document_viewer.js
@@ -8,7 +8,7 @@
       return tier;
     });
 
-    var width = 940;
+    var width = 620;
     var height = 500;
     var time_start = _.min(annotations, function(anno) { return anno.ts_ref1 }).ts_ref1;
     var time_end = _.max(annotations, function(anno) { return anno.ts_ref2}).ts_ref2;
