b151f9f4d3ddad9581fa312806e8bbe7e07241fa
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-03-05 16:53:46 -0800

    rearrange

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
 
 	validates :first_name, :last_name, :presence => 'true'
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
+
 end
