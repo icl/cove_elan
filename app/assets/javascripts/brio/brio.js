809d37b8d951023a3e67da44faf2e9f7dc26f50c
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-03-12 15:01:18 -0700

    zooming detail without zooming context

--- app/assets/javascripts/brio/brio.js
+++ app/assets/javascripts/brio/brio.js
@@ -5,10 +5,6 @@
   $.fn.conBrio = function( annotations, tiers) {
 var annotationsController = Ember.ArrayController.create();
 
-var context = Brio.Extent.create();
-
-var detail = Brio.Extent.create();
-
 var view_controller = Brio.ContextDetailController.create();
 
 var width = 1180;
@@ -16,41 +12,42 @@ var height = 100;
 
 var main_viewer = d3.select("#viewer").append('svg').attr('width', width).attr('height', height);
 
-var display = Brio.Extent.create({start: 0, end: width});
-
-
 //Scales
 
-var context_scale = Brio.Scale.create({domain: context, range: display});
-var detail_scale = Brio.Scale.create({domain: detail, range: display});
+
+view_controller.set('detail_x_scale', d3.scale.linear().domain([0,100]).range([0,1180]));
+view_controller.set('context_x_scale', d3.scale.linear().domain([0,100]).range([0,1180]));
+
 
 //var vertical_scale = d3.scale.linear.domain([0,100]).range([0,100]);
 
 function zoom() {
- console.warn(d3.event.translate);
 
- detail.set('start', detail.get('start') - d3.event.translate[0] / d3.event.scale);
- detail.set('end', detail.get('end') - d3.event.translate[0] / d3.event.scale);
 
- timeAxis.scale(detail_scale.get('scale'));
- main_viewer.select(".x.axis").call(timeAxis);
- console.warn("boom" + detail.get('start'));
- console.warn("dscale " + detail_scale.get('scale')(4));
+
+ main_viewer.select(".detail.axis").call(detailAxis);
+
+
 }
-//Context View
 
-detail.set('start', 0);
-detail.set('end', 1180);
 //Axis
 
-var test_scale = d3.scale.linear().domain([0,100000]).range([0,1180]);
 //zoom
-var zoomer = d3.behavior.zoom().x(test_scale).scaleExtent([1, 8]).on('zoom',zoom);
+var zoomer = d3.behavior.zoom().x(view_controller.get('detail_x_scale')).scaleExtent([1, 8]).on('zoom',zoom);
 
-var timeAxis = d3.svg.axis().scale(detail_scale.get('scale'));
+var detailAxis = d3.svg.axis().scale(view_controller.get('detail_x_scale'));
+
+var contextAxis = d3.svg.axis().scale(view_controller.get('context_x_scale'));
 
 //ScaleView
-var scale_view = main_viewer.append('g').attr('class', 'x axis').call(timeAxis);
+
+
+var detail_scale_view = main_viewer.append('g').attr('class', 'detail axis').call(detailAxis);
+var detail_scale_view = main_viewer.append('g')
+                                   .attr('class', 'context axis')
+                                   .attr("transform", "translate(0," + 40 + ")")
+                                   .call(contextAxis);
+
 
 main_viewer
         .call(zoomer);
