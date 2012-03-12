c51d2b543e2d8fb1726fab576241ca4fe7ff0075
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-03-12 15:37:55 -0700

    zooming between two scales with box to indicate detail area

--- app/assets/javascripts/brio/brio.js
+++ app/assets/javascripts/brio/brio.js
@@ -8,32 +8,30 @@ var annotationsController = Ember.ArrayController.create();
 var view_controller = Brio.ContextDetailController.create();
 
 var width = 1180;
-var height = 100;
+var height = 400;
 
 var main_viewer = d3.select("#viewer").append('svg').attr('width', width).attr('height', height);
 
 //Scales
 
 
-view_controller.set('detail_x_scale', d3.scale.linear().domain([0,100]).range([0,1180]));
-view_controller.set('context_x_scale', d3.scale.linear().domain([0,100]).range([0,1180]));
-
-
-//var vertical_scale = d3.scale.linear.domain([0,100]).range([0,100]);
+view_controller.set('detail_x_scale', d3.scale.linear().domain([0,1000]).range([0,1180]));
+view_controller.set('context_x_scale', d3.scale.linear().domain([0,1000]).range([0,1180]));
 
 function zoom() {
 
-
-
  main_viewer.select(".detail.axis").call(detailAxis);
-
-
+ main_viewer.select('rect.detail-area')
+           .attr('x', function() { return view_controller.get('detail_extent_in_context')[0]})
+           .attr('width', function() { 
+             return view_controller.get('detail_extent_in_context')[1] - 
+               view_controller.get('detail_extent_in_context')[0]});
 }
 
 //Axis
 
 //zoom
-var zoomer = d3.behavior.zoom().x(view_controller.get('detail_x_scale')).scaleExtent([1, 8]).on('zoom',zoom);
+var zoomer = d3.behavior.zoom().x(view_controller.get('detail_x_scale')).scaleExtent([1,120]).on('zoom',zoom);
 
 var detailAxis = d3.svg.axis().scale(view_controller.get('detail_x_scale'));
 
@@ -42,16 +40,30 @@ var contextAxis = d3.svg.axis().scale(view_controller.get('context_x_scale'));
 //ScaleView
 
 
-var detail_scale_view = main_viewer.append('g').attr('class', 'detail axis').call(detailAxis);
-var detail_scale_view = main_viewer.append('g')
+var context_scale_view = main_viewer.append('g')
                                    .attr('class', 'context axis')
                                    .attr("transform", "translate(0," + 40 + ")")
                                    .call(contextAxis);
 
+main_viewer.append('rect')
+           .attr('class', 'detail-area')
+           .attr('x', function() { return view_controller.get('detail_extent_in_context')[0]})
+           .attr('y', 40)
+           .attr('height', 20)
+           .attr('stroke', 'black')
+           .attr('fill', 'none')
+           .attr('width', function() { 
+             return view_controller.get('detail_extent_in_context')[1] - 
+               view_controller.get('detail_extent_in_context')[0]});
 
-main_viewer
-        .call(zoomer);
-//Detail View
+
+var detail_view = main_viewer.append('g').attr('class', 'detail-view').attr("transform", "translate(0," + 80 + ")")
+
+detail_view.call(zoomer);
+
+var detail_scale_view = detail_view.append('g').attr('class', 'detail axis').call(detailAxis);
+
+detail_view.append('rect').attr('x', 0).attr('y', 20).attr('width', 1180).attr('height', 300)
 //
   }
 })( jQuery );
--- app/assets/javascripts/brio/controllers/context_detail.js
+++ app/assets/javascripts/brio/controllers/context_detail.js
@@ -1,4 +1,8 @@
 Brio.ContextDetailController = Ember.Object.extend({
 
+ detail_extent_in_context: function() {
+    return [this.get('context_x_scale')(this.get('detail_x_scale').domain()[0]),
+            this.get('context_x_scale')(this.get('detail_x_scale').domain()[1])]
 
+ }.property()
 })
