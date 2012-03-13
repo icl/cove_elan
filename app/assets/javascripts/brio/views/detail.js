6bba54ab3db081a15bb5ee10b96c669cc9f0ed60
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-03-12 17:36:11 -0700

    mostly wired up detail view so its mostly self sufficient

--- app/assets/javascripts/brio/views/detail.js
+++ app/assets/javascripts/brio/views/detail.js
@@ -1,21 +1,28 @@
 Brio.DetailView = Ember.Object.extend({
+  init: function() {
+    this._super();
+    var self = this;
+    this.set('detail_view', this.get('parentView').append('g')
+        .attr('class', 'detail-view').attr("transform", "translate(0," + 80 + ")"));
+    this.set('zoom',  d3.behavior.zoom().x(self.getPath('controller.detail_x_scale'))
+                                 .on('zoom',function() {self.get('controller').receive_zoom()}));
+    this.set('axis', d3.svg.axis().scale(self.getPath('controller.detail_x_scale')));
 
+    this.set('detail_scale_view', self.get('detail_view').append('g').attr('class', 'detail axis'));
+  },
   update: function(){
     var self = this;
-  var zoomer = d3.behavior.zoom().x(view_controller.get('detail_x_scale'))
-  .on('zoom',function() {view_controller.receive_zoom()});
 
-  var detail_view = self.get('parentView').selectAll('g.detail-view').data([1]).enter().append('g')
-  .attr('class', 'detail-view').attr("transform", "translate(0," + 80 + ")")
 
-  detail_view.call(zoomer);
+  self.get('detail_view').call(self.get('zoom'));
   var bur = d3.svg.axis().scale(view_controller.get('detail_x_scale'));
-  var detail_scale_view = detail_view.append('g').attr('class', 'detail axis')
-  .call(bur);
+  self.get('detail_scale_view').call(self.get('axis'));
 
-  detail_view.append('rect').attr('x', 0).attr('y', 20).attr('width', 1180).attr('height', 300)
+  self.get('detail_view').append('rect').attr('x', 0)
+  .attr('y', 20).attr('width', 1180).attr('height', 300)
 
-  annos = detail_view.selectAll('rect.annotation').data(self.getPath('controller.annotations'))
+  annos = self.get('detail_view').selectAll('rect.annotation')
+                                 .data(self.getPath('controller.annotations'), function(d) {return d.id})
 
   annos.attr('x', function(d) { return self.getPath('controller.detail_x_scale')(d.ts_ref1)});
  
@@ -27,8 +34,6 @@ Brio.DetailView = Ember.Object.extend({
   .attr('height', 5)
   .attr('width', 5)
   .attr('stroke', 'blue');
-
-  annos.exit().remove();
 
 
   }.property('controller.scale', 'data')
