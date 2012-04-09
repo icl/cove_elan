b56549c7b8601c24a5a1f5cdf70056aec34fbaa8
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-04-09 16:20:34 -0700

    take out path authorization

--- app/models/capture.rb
+++ app/models/capture.rb
@@ -1,9 +1,28 @@
+require 'find'
+
 class Capture < ActiveRecord::Base
 
   validates_inclusion_of( :path, :in => ->(p){ Capture.authorized_paths  })
   
   has_many :user_assets
   belongs_to :user
+
+
+  def acquire_contents
+    if File.directory? self.path
+      sub_paths = Find.find(self.path)
+                      .reject {|path| File.directory? path }
+      sub_paths.each do |sub_path|
+        create_asset(file_name: File.open(sub_path))
+      end
+    else
+      create_asset(file_name: File.open(path))
+    end
+  end
+
+  def create_asset(attributes={})
+    self.user_assets << UserAsset.create(attributes)
+  end
 
   @@authorized_paths = []
 
@@ -20,4 +39,4 @@ class Capture < ActiveRecord::Base
   end
 
 
-  end
+end
--- spec/model/capture_spec.rb
+++ spec/model/capture_spec.rb
@@ -5,24 +5,44 @@ describe Capture do
   context "capturing only prespecified paths" do
     before :each do
       @capture_path = File.join(Rails.root, 'spec','fixtures')
-      
+
     end
 
     after :each do
       Capture.authorized_paths = []
     end
-    
+
     it "captures paths that it has been authorized too" do
+      pending
       Capture.authorize_path(@capture_path)
       c = Capture.create(path: @capture_path)
       c.should be_valid
     end
 
     it "doesn't capture paths that haven't been authorized" do
+      pending
       c = Capture.create(path: @capture_path)
       c.should_not be_valid
     end
 
+  end
+
+  context "capture a path full of files" do
+
+    before :all do
+      @capture_path = File.join(Rails.root, 'spec','fixtures')
+      Capture.authorize_path(@capture_path)
+    end
+
+    after :all do
+      Capture.authorized_paths = []
+    end
+
+    it "creates an Asset for each file" do
+      cap = Capture.create(path: @capture_path)
+      cap.acquire_contents
+      cap.user_assets.should_not be_empty
+    end
   end
 
   context "capturing a path that doesn't exist" 
