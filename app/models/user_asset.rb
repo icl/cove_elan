e162e83240b5dcaa0b6af212455c8f3299740b77
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-04-25 16:00:52 -0700

    3 column

--- app/models/user_asset.rb
+++ app/models/user_asset.rb
@@ -12,7 +12,7 @@ class UserAsset < ActiveRecord::Base
   has_many :meta_data_group_assignments, :as => :meta_data_group_assignable
   has_many :meta_data_groups, :through => :meta_data_group_assignments
 
-  has_many :meta_data_values, :as => :meta_data_valuable, :dependent => :destroy
+  has_many :meta_data_values, :as => :meta_data_valuable
 
   mount_uploader :file_name, UserAssetUploader
 
