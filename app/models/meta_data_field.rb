113cde2f82d8ea693b212c830a7ed79dfd4c7e37
Ethan Soutar-Rau <ethan.soutar.rau@gmail.com>
2012-04-27 16:44:44 -0700

    guard against too long field names, maybe that field should be text?

--- app/models/meta_data_field.rb
+++ app/models/meta_data_field.rb
@@ -1,6 +1,7 @@
 class MetaDataField < ActiveRecord::Base
   validates :meta_data_field_type, :name, :presence => true
 
+  validates_length_of :name, :maximum => 255
   belongs_to :meta_data_field_type
 
   has_many :meta_data_field_groups
@@ -9,4 +10,5 @@ class MetaDataField < ActiveRecord::Base
 	has_many :meta_data_values
 
   accepts_nested_attributes_for :meta_data_groups
+
 end
